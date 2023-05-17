418691a27236ec8f84dcc6bf5d27fb96ddafa284
Andrey Kudinov <2499931@gmail.com>
2023-05-17 15:54:06 +0400

    Add Netlify form

--- js/custom.js
+++ js/custom.js
@@ -15,3 +15,24 @@ function checkCookies(){
 	})
 }
 checkCookies();
+
+const handleSubmit = (event) => {
+	event.preventDefault();
+
+	const myForm = event.target;
+	const formData = new FormData(myForm);
+
+	console.log('work')
+
+	fetch("/", {
+		method: "POST",
+		headers: { "Content-Type": "application/x-www-form-urlencoded" },
+		body: new URLSearchParams(formData).toString(),
+	})
+		.then(() => console.log("Form successfully submitted"))
+		.catch((error) => alert(error));
+};
+
+document
+	.querySelector(".contact-us-form")
+	.addEventListener("submit", handleSubmit);
